69b6b0e091d0b30574b6419c37043717e7fa9643
ChenBexiuan <percyquispe88@gmail.com>
2025-06-24 14:12:02 -0500

    Update ManageApplications.jsx

--- src/pages/ManageApplications.jsx
+++ src/pages/ManageApplications.jsx
@@ -57,7 +57,7 @@ const ManageApplications = () => {
   // Función para ver el CV de forma segura
   const handleViewCv = async (fileName) => {
     try {
-        const response = await fetch(`http://localhost:8080/api/files/cvs/${fileName}`, {
+        const response = await fetch(`https://green-water-0ea21d31e.6.azurestaticapps.net/api/files/cvs/${fileName}`, {
             headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
         });
         if (!response.ok) throw new Error('No se pudo descargar el archivo.');
